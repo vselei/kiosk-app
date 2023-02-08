2b6bf212f945640e43f8c43f8fa38f5b6bdc1068
Victor Seleimend <vsennaselei@gmail.com>
2023-02-08 16:09:00 -0300

    layout

--- layout/Layout.js
+++ layout/Layout.js
@@ -8,10 +8,10 @@ const Layout = ({ children, page }) => {
         <meta name="description" content="Quiosque Cafeteria" />
       </Head>
       <div className='md:flex'>
-        <aside className='md:w-4/12'>
+        <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5'>
           <h1>Sidebar</h1>
         </aside>
-        <main className='md:w-8/12'>
+        <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
           {children}
         </main>
       </div>
