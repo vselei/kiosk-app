dca167e9c2138d07f150be62c1f316654513b5c3
Victor Seleimend <vsennaselei@gmail.com>
2023-02-08 14:01:51 -0300

    teste de requisição a db

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+export default function Home({ categories }) {
+  console.log(categories);
   return <h1>Hello World</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categories = await prisma.category.findMany();
+
+  console.log(categories);
+
+  return {
+    props: {
+      categories
+    }
+  };
+};
