2066db847d5f15788eaac8407507cb60adc42083
Victor Seleimend <vsennaselei@gmail.com>
2023-02-10 16:53:19 -0300

    registrando no banco de dados

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,6 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
 export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+
   if (req.method === 'POST') {
-    console.log(req.body);
-    res.json({ method: 'POST!' });
+    const order = await prisma.order.create({
+      data: {
+        order: req.body.order,
+        name: req.body.name,
+        total: req.body.total,
+        date: req.body.date
+      }
+    });
+
+    res.json(order);
   }
 }
